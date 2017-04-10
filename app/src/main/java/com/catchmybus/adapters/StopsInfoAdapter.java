1105b9fbdbbf6b23bea2bc4ae0afe3c2681f7bd1
Kevin Jayanthan <kevintjayan@gmail.com>
2017-04-10 16:23:24 +0530

    ADdadsd.

--- app/src/main/java/com/catchmybus/adapters/StopsInfoAdapter.java
+++ app/src/main/java/com/catchmybus/adapters/StopsInfoAdapter.java
@@ -9,6 +9,7 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.catchmybus.R;
+import com.catchmybus.settings.AppData;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -44,6 +45,10 @@ public class StopsInfoAdapter extends BaseAdapter {
                 this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View returnView = inflater.inflate(R.layout.single_stop_info, null);
         TextView stopNameText = (TextView) returnView.findViewById(R.id.stopName);
+
+
+
+
         TextView timeText = (TextView) returnView.findViewById(R.id.timeTaken);
         // Get the name of the stop at index i.
         try {
@@ -51,6 +56,14 @@ public class StopsInfoAdapter extends BaseAdapter {
             Log.i("RESPONSE", "Stop: " + stopName);
             stopNameText.setText(stopName);
             timeText.setText(this.stops.get(i).getString("time_to_source"));
+
+            if(AppData.get(this.context, "source_location_id").equals(
+                    this.stops.get(i).getString("location_id"))) {
+                    stopNameText.setTextColor(this.context.getColor(R.color.colorAccent));
+            } else if(AppData.get(this.context, "destination_location_id").equals(
+                    this.stops.get(i).getString("location_id")))  {
+                    stopNameText.setTextColor(this.context.getColor(R.color.colorAccent));
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
